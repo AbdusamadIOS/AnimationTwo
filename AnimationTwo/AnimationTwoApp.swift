1f68df7e45d13de07abe50086cffdb73318c6d06
Abdusamad <132121525+AbdusamadIOS@users.noreply.github.com>
2024-05-21 13:13:56 +0500

    Initial Commit

---
+++ AnimationTwo/AnimationTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationTwoApp.swift
+//  AnimationTwo
+//
+//  Created by Abdusamad Mamasoliyev on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
